876300e71510f82d52193c907fe61fdd491e3227
UmedaTakefumi <ut+github@azurestone.net>
2016-06-07 12:10:46 +0900

    python 学習コード

---
+++ python/test01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 文字列の出力
+print "test"
+print 'test'
+
+# ヒアドキュメント
+test_str = """
+test123
+test123
+hoge123
+"""
+
+print test_str
+
+# 文字列の連結
+test_str = "012"
+test_str += "345"
+test_str += "678"
+test_str += "9"
+
+print test_str
+
+
