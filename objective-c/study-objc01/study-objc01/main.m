44d06add2d81174be2343d10c5b7ff2de73c19c6
UmedaTakefumi <ut+github@azurestone.net>
2020-11-14 00:35:13 +0900

    study objc syntax

---
+++ objective-c/study-objc01/study-objc01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  study-objc01
+//
+//  Created by UmedaTakefumi on 2020/11/14.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
