c4c06b7968052de6edca09495545b1049a17e8b8
UmedaTakefumi <ut+github@azurestone.net>
2020-11-15 10:45:54 +0900

    obj-c

--- objective-c/study-objc01/study-objc01/main.m
+++ objective-c/study-objc01/study-objc01/main.m
@@ -11,6 +11,21 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        int foo;
+        foo = 0;
+        NSLog(@"%d", foo);
+    
+        int x = 0, y = 1024, z;
+        z = 11;
+        NSLog(@"%d, %d, %d ", x, y, z);
+        
+        NSString *foobar = @"hogehoge";
+        NSLog(@"%@", foobar);
+        
+        const int level = 0;
+        NSLog(@"%d", level);
+        
     }
+    
     return 0;
 }
