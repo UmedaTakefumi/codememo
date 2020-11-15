1dae0c0d980aaa54cb2444319bd9cc768a7a5cfe
UmedaTakefumi <ut+github@azurestone.net>
2020-11-16 03:26:21 +0900

    objc

--- objective-c/study-objc01/study-objc01/main.m
+++ objective-c/study-objc01/study-objc01/main.m
@@ -35,15 +35,28 @@ int main(int argc, const char * argv[]) {
     NSLog(@"%@", RESTAPI_URL);
     NSLog(@"%d", fuga(1, 2, 3, 4, 5));
     
-    enum {
+    
+    // ref:
+    //      https://sodocumentation.net/ja/objective-c/topic/1461/列挙型
+    typedef enum {
         NUM_A = 1,
         NUM_B = 2,
         NUM_C = 3,
         NUM_D = 4,
         NUM_E = 5,
         NUM_F = 6,
-        NUM_G = 7
-    };
+        NUM_G = 7,
+        
+    } NUMBER_PART;
     
+    NUMBER_PART SUUJI_01 = NUM_A;
+    NUMBER_PART SUUJI_02 = NUM_B;
+    
+    NSLog(@"+-------------------------------------+");
+    NSLog(@"%d", SUUJI_01);
+    NSLog(@"+-------------------------------------+");
+    NSLog(@"%d", SUUJI_02);
+    NSLog(@"+-------------------------------------+");
+
     return 0;
 }
