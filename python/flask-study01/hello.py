2240dc306b351e34bed8513bbe8a4e4c611c272d
UmedaTakefumi <ut+github@azurestone.net>
2020-11-26 00:00:17 +0900

    try pipenv run format

--- python/flask-study01/hello.py
+++ python/flask-study01/hello.py
@@ -2,18 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-  name = "Hello, World"
-  return name
+    name = "Hello, World"
+    return name
+
 
 @app.route('/good')
 def good():
-  name = "Good"
-  return name
+    name = "Good"
+    return name
+
 
 if __name__ == "__main__":
-	  app.run(debug=True, port=2000, host='0.0.0.0')
+    app.run(debug=True, port=2000, host='0.0.0.0')
 
-## ref:
-##   https://qiita.com/tomboyboy/items/122dfdb41188176e45b5
+# ref:
+# https://qiita.com/tomboyboy/items/122dfdb41188176e45b5
