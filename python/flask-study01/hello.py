e15af5b06b65510640ffcaf7d575c13bf50df8f8
UmedaTakefumi <ut+github@azurestone.net>
2020-11-25 23:29:24 +0900

    add flask-sample

---
+++ python/flask-study01/hello.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+  name = "Hello, World"
+  return name
+
+@app.route('/good')
+def good():
+  name = "Good"
+  return name
+
+if __name__ == "__main__":
+  app.run(debug=True, port=2000, host='0.0.0.0')
+
+## ref:
+##   https://qiita.com/tomboyboy/items/122dfdb41188176e45b5
