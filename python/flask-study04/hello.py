b5d74c5e8d6b0916f1cde1025e82632a19ddf67e
UmedaTakefumi <ut+github@azurestone.net>
2020-11-29 21:54:44 +0900

    add study-flask

---
+++ python/flask-study04/hello.py
@@ -0,0 +1,27 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def odd_even():
+	if request.method == "GET":
+		return """
+		下に整数を入力してください。奇数か偶数か判定します
+		<form action="/" method="POST">
+		<input name="num"></input>
+		</form>"""
+	else:
+		return """
+		{}は{}です！
+	        <form action="/" method="POST">
+        	<input name="num"></input>
+        	</form>""".format(str(request.form["num"]), ["偶数", "奇数"][int(request.form["num"]) % 2])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# ref:
+#   https://qiita.com/tomboyboy/items/122dfdb41188176e45b5
+#   https://qiita.com/nagataaaas/items/d249c3905d41137cd510
+#   https://qiita.com/nagataaaas/items/3116352da186df102d96
+
