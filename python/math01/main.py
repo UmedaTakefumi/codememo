cb5c42ca347fe6bee6226ef6f7200b1c11ff8ab4
UmedaTakefumi <ut+github@azurestone.net>
2020-12-19 15:55:57 +0900

    aaa

--- python/math01/main.py
+++ python/math01/main.py
@@ -2,8 +2,22 @@
 
 import math
 
+#i = 12345.5
+#print(math.ceil(i))
 
 
-i = 12345.5
-print(math.ceil(i))
+def compute():
 
+    for i in range(1, 6):
+        print(i)
+    
+    #for k in range(0.01, 1):
+    #    print(k)
+
+    print("aaa")
+
+if __name__ == "__main__":
+
+
+    
+    compute()
