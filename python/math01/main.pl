4396307c65cbda5ffc8eb6fda0835012d48d8780
UmedaTakefumi <ut+github@azurestone.net>
2020-12-19 16:16:35 +0900

    a

--- python/math01/main.pl
+++ python/math01/main.pl
@@ -1,10 +1,27 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl -l
 
 use strict;
 use warnings;
 use Data::Dumper;
 
-foreach my $tmp (0.01..1) {
-  print "$tmp\n";
+#foreach my $tmp (0.01..1) {
+#  print "$tmp\n";
+#
+#}
 
+#for (my $t = 0.01; $t = $t+0.01; $t<=1.0) {
+#  print $t
+#}
+
+for (my $t = 0.01; $t<=1.0; $t = $t+0.01) {
+  print $t
 }
+print "+-+-+-+-+-";
+
+for (my $t = 0.01; $t<=1; $t = $t+0.01) {
+  print $t
+}
+#for (my $t = 0; $t<=6; $t++) {
+#  print $t
+#}
+
