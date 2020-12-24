7c6eecbb4ca0e0ba4f6280bca1637541a94b58a7
UmedaTakefumi <ut+github@azurestone.net>
2020-12-24 23:37:19 +0900

    add build docker script

---
+++ 2020.12.19_loop_add_float/docker-build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DOCKER_TAG="ss-$(pwd | awk -F/ '{ print $NF }')"
+
+function build () {
+  docker build -t $DOCKER_TAG .
+}
+
+function login () {
+  docker run -it $DOCKER_TAG /bin/bash
+}
+
+function root_login () {
+  docker exec -it --user root $DOCKER_TAG /bin/bash
+}
+
+if	[ $1 = "build"  ]; then
+  build
+elif [ $1 = "login" ]; then
+  login
+elif [ $1 = "root_login" ]; then
+  root_login	
+fi
+
