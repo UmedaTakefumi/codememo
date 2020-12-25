a1668edf6056b985bc7c1d27141005bdde1643fd
UmedaTakefumi <ut+github@azurestone.net>
2020-12-26 06:20:44 +0900

    add docker script

---
+++ 2020.11.28_wsgi-samples/operate-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DOCKER_TAG="$(pwd | awk -F/ '{ print $NF }')"
+
+function __build () {
+  docker build -t $DOCKER_TAG .
+}
+
+function __login () {
+  docker run -it $DOCKER_TAG /bin/bash
+}
+
+function __root_login () {
+  docker exec -it --user root $DOCKER_TAG /bin/bash
+}
+
+function usage_exit() {
+  echo -e "Usage: $0 \n" 1>&2
+  declare -F | awk '{ print " " $3 }' | grep "__"
+  echo -e "\n"
+}
+
+if [ "$1" = "build"  ]; then
+  build
+elif [ "$1" = "login" ]; then
+  login
+elif [ "$1" = "root_login" ]; then
+  root_login
+elif [ "$1" = "" ]; then
+  usage_exit
+else
+  usage_exit
+fi
+
